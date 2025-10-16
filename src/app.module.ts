9c36a8bde1c28a163a96e1301f7c4d419b7685db
Maycon Marques <mayconmarquesh@gmail.com>
2025-10-16 13:41:24 -0300

    catalog resource init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatalogController } from './catalog/catalog.controller';
+import { CatalogService } from './catalog/catalog.service';
+import { CatalogModule } from './catalog/catalog.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CatalogModule, ConfigModule.forRoot({ isGlobal: true })],
+  controllers: [AppController, CatalogController],
+  providers: [AppService, CatalogService],
 })
 export class AppModule {}
